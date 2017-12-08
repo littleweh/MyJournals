51a9a2ecd82ef639bd79a136f234cc6fcd8898d3
littleweh <adakao@gmail.com>
2017-12-08 12:43:54 +0800

    add gradient layer

--- MyJournals/MyJournals/EntryCreatingViewController.swift
+++ MyJournals/MyJournals/EntryCreatingViewController.swift
@@ -12,12 +12,14 @@ class EntryCreatingViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var journalTitleTextField: UITextField!
-    @IBOutlet weak var contentTextField: UITextField!
     @IBOutlet weak var buttonContainerView: UIView!
+    @IBOutlet weak var contentTextView: UITextView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
+    let gradientLayer = CAGradientLayer()
 
-
+    @IBOutlet weak var contentTextViewHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         cancelButton.addTarget(
@@ -26,7 +28,20 @@ class EntryCreatingViewController: UIViewController {
             for: .touchUpInside
         )
 
+        saveButton.addTarget(
+            self,
+            action: #selector(saveInCoreData),
+            for: .touchUpInside
+        )
+
+        setupUI()
+
         // Do any additional setup after loading the view.
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = imageView.bounds
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,19 +49,37 @@ class EntryCreatingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func saveInCoreData(){
+
+    }
+
     @objc func cancelBackEntryList() {
         dismiss(animated: true, completion: nil)
     }
+
+    func setupUI() {
+
+        setupImageGradientView()
+
+        contentTextView.sizeToFit()
+        contentTextView.layoutIfNeeded()
+        contentTextViewHeightConstraint.constant = contentTextView.intrinsicContentSize.height
+        
+    }
+
+    private func setupImageGradientView() {
+
+        gradientLayer.colors = [
+            UIColor(red: 67.0 / 255.0, green: 87.0 / 255.0, blue: 97.0 / 255.0, alpha: 1.0).cgColor,
+            UIColor(red: 26.0 / 255.0, green: 34.0 / 255.0, blue: 38.0 / 255.0, alpha: 1.0).cgColor
+        ]
+
+        imageView.layer.addSublayer(gradientLayer)
+
+    }
+
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
